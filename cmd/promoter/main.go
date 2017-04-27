b7b6e4d8cb412b7c54b8b8ad0cf032a10fabaee2
Federico Gimenez <fgimenez@canonical.com>
2017-04-27 13:22:17 +0200

    add in-memory repo and extend checks for created promotions

--- cmd/promoter/main.go
+++ cmd/promoter/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/unrolled/render"
@@ -10,15 +11,27 @@ func main() {
 }
 
 type promotion struct {
-	ID string `json:"id"`
+	ID   string `json:"id"`
+	Snap string `json:"snap"`
 }
 
-func createPromotionHandler(formatter *render.Render) http.HandlerFunc {
+type Persister interface {
+	AddPromotion(*promotion) string
+	GetPromotion(string) *promotion
+}
+
+func createPromotionHandler(formatter *render.Render, repo Persister) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
+		dec := json.NewDecoder(req.Body)
+		p := promotion{}
+		dec.Decode(&p)
+		id := repo.AddPromotion(&p)
 		w.Header().Add("Location", "/promotions/1")
 		formatter.JSON(w,
 			http.StatusCreated,
-			&promotion{ID: "1"},
+			&promotion{
+				ID: id,
+			},
 		)
 	}
 }
